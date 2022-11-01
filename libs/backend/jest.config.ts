d8a386e04c04f1b91113257cca0597611ea07a19
철철 <zl6863vk@gmail.com>
2022-11-01 22:56:41 +0900

    Project settings (#2)
    * refactor(package): rename default project
    - cheol-notion -> backend-apis
    
    * refactor(package): script 수정
    
    * feat(libs/backend): libs/backend init
    
    * feat(deploy): local setting 관련 docker-compose 작성

--- libs/backend/jest.config.ts
+++ libs/backend/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'cheol-notion',
+  displayName: 'backend',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/cheol-notion',
+  coverageDirectory: '../../coverage/libs/backend',
 };
